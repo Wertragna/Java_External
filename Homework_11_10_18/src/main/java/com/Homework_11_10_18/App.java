473d859fb341248db948a8eb4cb9be79860a20e7
Bohdana Sherstyniuk <ms.sherstynyuk@gmail.com>
2018-10-14 17:21:38 +0300

    Initial commit homework(11.10.18)

---
+++ Homework_11_10_18/src/main/java/com/Homework_11_10_18/App.java
@@ -0,0 +1,13 @@
+package com.Homework_11_10_18;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
